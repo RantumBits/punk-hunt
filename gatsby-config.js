65de08aee3d66d645bd55827eb626b77ea50645a
andrew maury <andrew@ecomloop.com>
2021-04-11 11:58:55 -0700

    add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,21 @@
 module.exports = {
   siteMetadata: {
     title: 'PunkHunt | Find them All',
-    author: 'RantumBits @ecomloop',
+    author: '@RantumBits',
     description: 'Complete all the CryptoPunk challenges during the Miami billboard takeover',
+    image: 'src/images/punkhunt_billboard.png',
+    url: 'https://PunkHunt.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'PunkHunt | Find them all',
+        short_name: 'punkhunt',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#fff',
+        theme_color: '#ff04b4',
         display: 'minimal-ui',
         icon: 'src/images/rocket.png', // This path is relative to the root of the site.
       },
